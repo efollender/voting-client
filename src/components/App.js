9a47147d9a071f0f58b33b331d2d55c66d8a39eb
Emma Follender <efollender@gmail.com>
2016-01-26 00:32:13 -0500

    Added redux. Added fake tally

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
+
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				{React.cloneElement(this.props.children, {pair: pair})}
+				{React.cloneElement(this.props.children, {
+					pair: pair,
+					tally: tally
+				})}
 			</div>
 		);
 	}
--- src/components/Results.js
+++ src/components/Results.js
@@ -2,12 +2,32 @@ import React, {Component, PropTypes} from 'react';
 import {shouldPureComponentUpdate} from 'react-pure-render';
 
 export default class Results extends Component {
-	shouldComponentUpdate = shouldPureComponentUpdate;
-	render() {
-		return (
-			<div>
-				Results!
-			</div>
-		);
-	}
+  shouldComponentUpdate = shouldPureComponentUpdate;
+  static propTypes = {
+    pair: PropTypes.array,
+    tally: PropTypes.object
+  };
+  getPair() {
+    return this.props.pair || [];
+  }
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  }
+  render() {
+    return (
+      <div className="results">
+        {this.getPair().map(entry =>
+          <div key={entry} className="entry">
+            <h1>{entry}</h1>
+            <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
