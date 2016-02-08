2c70f39fdbbacc0b2fa1e9abe0e2ae89e63f8ae6
Emma Follender <follender@brooklynunited.com>
2016-02-08 11:20:05 -0500

    Added mobile styles for happenings

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 import {List, Map} from 'immutable';
 
-const pair = List.of('Trainspotting', '28 Days Later');
-const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
-
-
 export default class App extends Component {
 	render() {
 		return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import Routes from './routes';
-import * as ui from './constants';
 import {store} from './store';
 
 ReactDOM.render(
